39eb314c610b838782961a75a42d93831da86ce5
pattarapol sawasdee <pattarapol.s@kbtg.tech>
2019-06-29 10:45:16 +0700

    change to return func

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/Top-Pattarapol/school-service/service"
@@ -10,17 +11,19 @@ import (
 )
 
 func main() {
-	r := gin.Default()
-	setPath(r)
+	r := serRoute()
 	r.Run(getPort())
 }
 
-func setPath(r *gin.Engine) {
+func serRoute() *gin.Engine {
+	r := gin.Default()
+
 	r.GET("/api/todos", service.GetTodos)
 	r.GET("/api/todos/:id", service.GetTodosById)
 	r.POST("/api/todos/", service.PostTodos)
 	r.DELETE("/api/todos/:id", service.DeleteTodosById)
 	r.PUT("/api/todos/:id", service.UpdateTodo)
+	return r
 }
 
 func getPort() string {
